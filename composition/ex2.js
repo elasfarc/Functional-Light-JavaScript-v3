4e8c40792ffcf7b931bde379b57ae83564e082d5
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-02-28 12:42:37 +0200

    [composition] Add general utility functions compose(..) && pipe(..)

--- composition/ex2.js
+++ composition/ex2.js
@@ -1,18 +1,4 @@
 "use strict";
-
-function increment(x) {
-  return x + 1;
-}
-function decrement(x) {
-  return x - 1;
-}
-function double(x) {
-  return x * 2;
-}
-function half(x) {
-  return x / 2;
-}
-
 var f1 = compose(increment, decrement);
 var f2 = pipe(decrement, increment);
 var f3 = compose(decrement, double, increment, half);
@@ -26,3 +12,48 @@ console.log(f3(3) === 4);
 console.log(f3(3) === f4(3));
 console.log(f5(3) === 4);
 console.log(f5(3) === f6(3));
+
+//***********************/
+function increment(x) {
+  return x + 1;
+}
+function decrement(x) {
+  return x - 1;
+}
+function double(x) {
+  return x * 2;
+}
+function half(x) {
+  return x / 2;
+}
+
+function compose(...fns) {
+  return function composed(v) {
+    return fns.reduceRight(composeReducer, v);
+    //****/
+    function composeReducer(result, fn) {
+      return fn(result);
+    }
+  };
+}
+
+function pipe(...fns) {
+  return compose(unApply(compose), reverseArr)(fns);
+  //return compose(unapply(compose), Array.prototype.reverse.bind(fns))(fns);
+  //   return compose(unapply(compose))(fns.reverse());
+  //   return compose(...fns.reverse());
+}
+
+function apply(fn) {
+  return function (...args) {
+    return fn(args);
+  };
+}
+function unApply(fn) {
+  return function (arr) {
+    return fn(...arr);
+  };
+}
+function reverseArr(arr) {
+  return arr.reverse();
+}

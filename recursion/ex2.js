c69da647acac081dfc9980d0cf464cd69b5845e0
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-03-01 14:07:06 +0200

    [recursion] Add a recursive isPalindrome(..) function

--- recursion/ex2.js
+++ recursion/ex2.js
@@ -1,6 +1,15 @@
 "use strict";
+const R = require("ramda");
 
-function isPalindrome() {}
+function isPalindrome(str) {
+  var isFirstEqLast = (str) => R.equals(R.head(str))(R.last(str));
+  return R.defaultTo(false)(
+    R.cond([
+      [R.either(R.isEmpty, R.compose(R.equals(1), R.length)), R.T],
+      [isFirstEqLast, R.compose(isPalindrome, R.compose(R.init, R.tail))],
+    ])(str)
+  );
+}
 
 console.log(isPalindrome("") === true);
 console.log(isPalindrome("a") === true);
